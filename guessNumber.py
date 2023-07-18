8121cd3bab9fe85bec4e7d8d3c3527b29b72c741
jodosjodos <jeandedieu2030@gmail.com>
2023-07-18 12:43:19 +0200

    finishing madlibs and guess number for both computer and user

--- guessNumber.py
+++ guessNumber.py
@@ -13,3 +13,21 @@ def guess(x):
 
 
 guess(20)
+
+def computer_guess(x):
+    low=1
+    high=x
+    feedback=''
+    while feedback !='c' :
+        if low!=high :
+         guess=random.randint(low,high)
+        else :
+            guess=high    
+        feedback=input(f" is {guess} to high(H) , to low(L) or correct (C)  : ").lower()
+        if feedback=="h":
+            high=guess-1
+        elif feedback=='l':
+            low=guess+1    
+    print(f" Yay ! Compute guessed your number , {guess}, correctly ")
+    
+computer_guess(20)            
